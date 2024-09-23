d31a8ef984029156b32849d0da536d1aefe02a42
M_Αmin Jafarnejad <mohammadamindf@gmail.com>
2024-09-23 10:53:00 +0330

    Fix : fix some condition and style
    Add click event on (buttons), fix height style in CSS, and add some condition for all tree colors

--- js/script.js
+++ js/script.js
@@ -9,6 +9,10 @@ function plus(){
 if(spinnerValue >= 10){
     return false
 }
+if(spinnerValue>=4){
+meter.style.background="rgb(211, 211, 9)"
+meter.style.filter="drop-shadow(0 0 2.5px rgb(229, 255, 0)) drop-shadow(0 0 10px rgb(229, 255, 0))"
+}
 if(spinnerValue >= 7 ){
     meter.style.background = "#f00"
     meter.style.filter = "drop-shadow(0 0 2.5px #f00) drop-shadow(0 0 10px #f00)"
@@ -17,13 +21,20 @@ output.innerHTML = ++spinnerValue;
 meter.style.height = spinnerValue*10+"%"
 }
 function minus(){
-    if(spinnerValue<= 0 ){
-        return false;
-    }
+  
     if(spinnerValue <= 7 ){
-        meter.style.background = "#0f0"
+        meter.style.background="rgb(211, 211, 9)"
+        meter.style.filter="drop-shadow(0 0 2.5px rgb(229, 255, 0)) drop-shadow(0 0 10px rgb(229, 255, 0))"
+            }
+    if(spinnerValue<=4){
+                meter.style.background = "#0f0"
         meter.style.filter = "drop-shadow(0 0 2.5px #0f0) drop-shadow(0 0 10px #0f0)"
-    }
-    output.innerHTML= --x;
-    meter.style.height = x*10+'%' ;
+
+        }
+        if(spinnerValue<= 0 ){
+            return false;
+        }
+
+    output.innerHTML= --spinnerValue;
+    meter.style.height = spinnerValue*10+'%' ;
 }
