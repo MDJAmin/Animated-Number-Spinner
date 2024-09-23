cfd6dd3b0f1c634b31d48a3a1aad5094bbce8320
M_Αmin Jafarnejad <mohammadamindf@gmail.com>
2024-09-23 10:18:31 +0330

    Refactor : add plus function

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
 "use strict"
+
+let spinnerValue = 0; 
+let output = document.querySelector("#output")
+let meter = document.querySelector("#meter")
+output.innerHTML=spinnerValue;
+
+function plus(){
+if(spinnerValue >= 10){
+    return false
+}
+if(spinnerValue >= 7 ){
+    meter.style.background = "#f00"
+    meter.style.filter = "drop-shadow(0 0 2.5px #f00) drop-shadow(0 0 10px #f00)"
+}
+output.innerHTML = ++spinnerValue;
+meter.style.height = spinnerValue*10+"%"
+}
+
